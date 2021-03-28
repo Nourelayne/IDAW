05cddb5d56a7f17a463195e97c44d4aaf053b953
Salah Eddine Nourelayne <nourelaynesalaheddine@gmail.com>
2021-03-28 23:45:00 +0200

    TP4 Finished

--- TP4_Part1/app.js
+++ TP4_Part1/app.js
@@ -1,93 +1,121 @@
 let studentMaxId = 0;
 let students = [];
-
+let selectedRow = null;
+let currentId = 0;
 
 function onFormSubmit() {
 
-    // Disable the sending of the form
     event.preventDefault();
+
+    if (valideForm()) {
+        let newStudent = readInputs();
+
+        if (selectedRow == null){
+            insertStudent(newStudent);
+            students.push(newStudent);
+        }
+        else{
+            newStudent.id = currentId;
+            students[currentId] = newStudent;
+            saveStudent(newStudent);
+        }
+        clearFields();
+    }
+    console.log(students);
+}
+
+function valideForm(){
+    let valid = true;
+    if (document.getElementById("nom").value == "") {
+        document.getElementById("nom").style.borderColor = "red";
+        document.getElementById("error").innerHTML = "Veuillez entrer votre nom";
+        document.getElementById("error").style.color = "red";
+        valid = false;
+    }
+    return valid;
+}
+
+function readInputs(){
 
     let newStudent = {};
 
-    newStudent.nom = $('#nom').val();
-    newStudent.prenom = $('#prenom').val();
-    newStudent.date = $('#date').val();
-    newStudent.check = $('#check').prop("checked") ? "oui" : "non";
-    newStudent.note = $('#note').val();
+    newStudent.nom = document.getElementById("nom").value;
+    newStudent.prenom = document.getElementById("prenom").value;
+    newStudent.date = document.getElementById("date").value;
+    newStudent.check = document.getElementById("check").checked ? "oui" : "non";
+    newStudent.note = document.getElementById("note").value;
     newStudent.id = studentMaxId;
     studentMaxId ++;
 
-    // Checking if the name is valid
-    if (newStudent.nom === "") {
-        nom.style.borderColor = "red";
-        error.innerHTML = "Veuillez entrer votre nom";
-        error.style.color = "red";
-        return;
-    }
+    return newStudent;
+}
 
-    // Adding data to the table
-    let template = `
-                            <tr>
+function insertStudent(newStudent){
+    let table = document.getElementById("studentList");
+    table.innerHTML += 
+                        `
+                            <tr class="tr">
                                 <td>${newStudent.nom}</td>
                                 <td>${newStudent.prenom}</td>
                                 <td>${newStudent.date}</td>
                                 <td>${newStudent.check}</td>
                                 <td>${newStudent.note}</td>
                                 <td>
-                                    <button type="submit" class="btn btn-secondary" id="saveButton" style="display: none;" onclick= "save(${newStudent.id})">Save</button>
-                                    <button type="submit" class="btn btn-primary" id="editButton" onclick= "editRow(${newStudent.id})">Modifier</button>
-                                    <button type="submit" class="btn btn-primary" id="removeButton" onclick= "removeRow(${newStudent.id})">Supprimer</button>
+                                    <button type="submit" class="btn btn-primary" id="editButton" onclick= "editStudent(${newStudent.id})">Modifier</button>
+                                    <button type="submit" class="btn btn-primary" id="removeButton" onclick= "removeStudent(${newStudent.id})">Supprimer</button>
                                 </td>
                             </tr>
-                        `
-    document.querySelector("table").innerHTML += template;
-    students.push(newStudent);
-    console.log(students);
-
-    // Clear fields
-    $('#nom').val("");
-    $('#prenom').val("");
-    $('#date').val("");
-    $('#check').prop("checked",false);
-    $('#note').val("");
-    $('#nom').css("border-color", null);
-    $('#error').html("");
-    $('#error').css("color", null);
+                        `;
 }
 
-function removeRow(id) {
+function removeStudent(id) {
     students = students.filter(student => student.id !== id);
+    students.map(student =>{
+        if(student.id > id){
+            student.id-- ;
+        }
+    });
     event.target.closest("tr").remove();
 }
 
-function editRow(id) {
-    $("#saveButton").css("display" , "inline-block");
-    $("#editButton").css("display" , "none");
+function editStudent(id) {
+    selectedRow = event.target.closest("tr");
 
-    $('#nom').val(students[id].nom);
-    $('#prenom').val(students[id].prenom);
-    $('#date').val(students[id].date);
-    $('#check').prop("checked", students[id].check === "oui" ? true : false);
-    $('#note').val(students[id].note);
+    document.getElementById("editButton").disabled = true;
+    document.getElementById("removeButton").disabled = true;
+
+    document.getElementById("nom").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("prenom").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("date").value = selectedRow.cells[2].innerHTML;
+    selectedRow.cells[3].innerHTML === "oui" ? document.getElementById("check").checked = true : document.getElementById("check").checked = false;
+    document.getElementById("note").value = selectedRow.cells[4].innerHTML;
+
+    currentId = id;
 }
 
-function save(id){
-    event.target.closest("tr").cells[0].innerHTML = students[id].nom = $('#nom').val();
-    event.target.closest("tr").cells[1].innerHTML = students[id].prenom = $('#prenom').val();
-    event.target.closest("tr").cells[2].innerHTML = students[id].date = $('#date').val();
-    event.target.closest("tr").cells[3].innerHTML = students[id].check =  $('#check').prop("checked") ? "oui" : "non";
-    event.target.closest("tr").cells[4].innerHTML = students[id].note = $('#note').val();
+function saveStudent(student) {
+    document.getElementById("editButton").disabled = false;
+    document.getElementById("removeButton").disabled = false;
 
-    $("#editButton").css("display" , "inline-block");
-    $("#saveButton").css("display" , "none");
-
-    $('#nom').val("");
-    $('#prenom').val("");
-    $('#date').val("");
-    $('#check').prop("checked",false);
-    $('#note').val("");
-    $('#nom').css("border-color", null);
-    $('#error').html("");
-    $('#error').css("color", null);
-    console.log(students);
+    selectedRow.cells[0].innerHTML = student.nom;
+    selectedRow.cells[1].innerHTML = student.prenom;
+    selectedRow.cells[2].innerHTML = student.date;
+    selectedRow.cells[3].innerHTML = student.check;
+    selectedRow.cells[4].innerHTML = student.note;
 }
+
+function clearFields(){
+    document.getElementById("nom").value = "";
+    document.getElementById("prenom").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("check").checked = false;
+    document.getElementById("note").value = "";
+}
+
+function clearError(){
+    document.getElementById("nom").style.borderColor = null;
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("error").style.color = null;
+}
+
+
