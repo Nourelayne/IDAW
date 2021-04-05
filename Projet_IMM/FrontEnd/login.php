78f8f3d02051ab2ce0d77e0f95c2452c149ee70a
Salah Eddine Nourelayne <nourelaynesalaheddine@gmail.com>
2021-04-05 22:53:31 +0200

    Done: 99%

--- Projet_IMM/FrontEnd/login.php
+++ Projet_IMM/FrontEnd/login.php
@@ -3,6 +3,7 @@
       <div id="form-container">
         <form action="\IDAW\Projet_IMM\BackEnd\connexionTest.php" method="POST">
           <h1>Se connecter</h1>
+          <pre>Email: email de l'école, Password: "${nom}123"</pre>
           <input type="email" placeholder="Login" name="login" required />
           <input type="password" placeholder="Password" name="password" required />
           <button id="signIn">Sign In</button>
